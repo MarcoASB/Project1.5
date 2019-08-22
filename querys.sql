e78d58d92e03558647206debdfc9a536a2a7efbc
Marco Solorzano Baez <marsolbae@gmail.com>
2019-08-21 20:10:33 -0500

    Python and Querys incomplete

---
+++ querys.sql
@@ -0,0 +1,69 @@
+CREATE TABLE Barcelona_1992 (
+id INT PRIMARY KEY,
+country TEXT,
+country_id TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT
+);
+
+CREATE TABLE Atlanta_1996 (
+id INT PRIMARY KEY,
+country TEXT,
+country_id TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT
+);
+
+CREATE TABLE Sidney_2000 (
+id INT PRIMARY KEY,
+country TEXT,
+country_id TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT
+);
+
+CREATE TABLE Atenas_2004 (
+id INT PRIMARY KEY,
+country TEXT,
+country_id TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT
+);
+
+CREATE TABLE Pekin_2008 (
+id INT PRIMARY KEY,
+country TEXT,
+country_id TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT
+);
+
+CREATE TABLE Londres_2012 (
+id INT PRIMARY KEY,
+country TEXT,
+country_id TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT
+);
+
+CREATE TABLE Rio_2016 (
+id INT PRIMARY KEY,
+country TEXT,
+country_id TEXT,
+Gold INT,
+Silver INT,
+Bronze INT,
+Total INT
+);
