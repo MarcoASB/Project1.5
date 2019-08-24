dbb243878b8e167d0acf7c22d55ad7e4b1163d70
Fernandarangel23 <48704755+Fernandarangel23@users.noreply.github.com>
2019-08-24 09:58:36 -0500

    Add files via upload

---
+++ Project1.5/querys.sql
@@ -0,0 +1,105 @@
+CREATE TABLE barcelona_1992 (
+country TEXT,
+country_id TEXT,
+gold_92 INT,
+silver_92 INT,
+bronze_92 INT,
+total_92 INT
+);
+
+CREATE TABLE atlanta_1996 (
+country TEXT,
+country_id TEXT,
+gold_96 INT,
+silver_96 INT,
+bronze_96 INT,
+total_96 INT
+);
+
+CREATE TABLE sidney_2000 (
+country TEXT,
+country_id TEXT,
+gold_00 INT,
+silver_00 INT,
+bronze_00 INT,
+total_00 INT
+);
+
+CREATE TABLE atenas_2004 (
+country TEXT,
+country_id TEXT,
+gold_04 INT,
+silver_04 INT,
+bronze_04 INT,
+total_04 INT
+);
+
+CREATE TABLE pekin_2008 (
+country TEXT,
+country_id TEXT,
+gold_08 INT,
+silver_08 INT,
+bronze_08 INT,
+total_08 INT
+);
+
+CREATE TABLE londres_2012 (
+country TEXT,
+country_id TEXT,
+gold_12 INT,
+silver_12 INT,
+bronze_12 INT,
+total_12 INT
+);
+
+
+CREATE TABLE rio_2016 (
+country TEXT,
+country_id TEXT,
+gold_16 INT,
+silver_16 INT,
+bronze_16 INT,
+total_16 INT
+);
+
+CREATE TABLE gold (
+country TEXT,
+country_id TEXT,
+gold_92 INT,
+gold_96 INT,
+gold_00 INT,
+gold_04 INT,
+gold_08 INT,
+gold_12 INT,
+gold_16 INT,
+total_gold INT
+);
+
+CREATE TABLE silver (
+country TEXT,
+country_id TEXT,
+silver_92 INT,
+silver_96 INT,
+silver_00 INT,
+silver_04 INT,
+silver_08 INT,
+silver_12 INT,
+silver_16 INT,
+total_silver INT
+);
+
+CREATE TABLE bronze (
+country TEXT,
+country_id TEXT,
+bronze_92 INT,
+bronze_96 INT,
+bronze_00 INT,
+bronze_04 INT,
+bronze_08 INT,
+bronze_12 INT,
+bronze_16 INT,
+total_bronze INT
+);
+
+select * from silver 
+;
